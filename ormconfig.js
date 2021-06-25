bfc30b3874ec9a77e1ff9e5002cdf7682bc7ffd5
Rafael Gaitan <rafael.tgt@gmail.com>
2021-06-25 12:05:17 -0300

    configs to heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
-{
-    "type": "sqlite",
-    "database": "src/database/database.sqlite",
+module.exports = {
+    "type": "postgres",
+    "url": process.env.DATABASE_URL,
     "migrations": [
-        "src/database/migrations/*.ts"
+        "build/database/migrations/*.js"
     ],
     "entities": [
-        "src/entities/*.ts"
+        "build/entities/*.js"
     ],
     "cli": {
         "migrationsDir": "src/database/migrations",
